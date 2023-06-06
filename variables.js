cb0410a2cf1331dc1de0d904d500401de5c075c8
Sara Connelly <sara.k.connelly@gmail.com>
2023-06-06 13:04:02 -0600

    Add console log statements

--- variables.js
+++ variables.js
@@ -7,3 +7,12 @@ var height = 187.96
 
 var rentPaid = true
 var groceryShop = true
+
+pet = "Lou Lou"
+email = "skendallc@gmail.com"
+couch = "green"
+
+console.log(pet)
+console.log(email)
+console.log(age)
+console.log(rentPaid)
